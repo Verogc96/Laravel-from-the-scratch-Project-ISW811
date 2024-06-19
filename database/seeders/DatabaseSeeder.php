f6f93a91151f7320bb8c57866dcb58f622553d72
Veronica Gutierrez <verogc96@gmail.com>
2024-06-18 18:56:41 -0600

    Turbo Boost With Factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,43 +16,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::truncate();
-        Category::truncate();
-        Post::truncate();
+       $user = User::factory()->create([
+        'name' => 'John Doe'
+       ]);
 
-        $user= User::factory()->create();
-
-        $personal = Category::create([
-            'name'=> 'Personal',
-            'slug'=>'personal',
-        ]);
-
-        $family = Category::create([
-            'name'=> 'Family',
-            'slug'=>'family',
-        ]);
-
-        $work = Category::create([
-            'name'=> 'Work',
-            'slug'=>'work',
-        ]);
-
-        Post::create([
-            'user_id'=> $user->id,
-            'category_id'=> $family->id,
-            'title'=> 'My Family Post',
-            'slug'=>'my-first-post',
-            'excerpt'=>'Lorem ipsum dolar sit amet.',
-            'body'=>'<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent semper, eros id placerat auctor, metus lorem sodales justo, non facilisis sapien enim sit amet tortor. Phasellus sagittis erat blandit, ultrices eros vel, ultrices mi. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vestibulum ipsum mauris, venenatis non condimentum a, ultricies quis diam. Etiam commodo sodales tellus id faucibus. Maecenas dignissim, justo vel cursus viverra, felis augue lacinia eros, id ullamcorper erat ex a mi. Mauris non nulla dignissim quam ultrices tincidunt vitae ac elit.</p>',
-        ]);
-
-        Post::create([
-            'user_id'=> $user->id,
-            'category_id'=> $family->id,
-            'title'=> 'My Work Post',
-            'slug'=>'my-work-post',
-            'excerpt'=>'Lorem ipsum dolar sit amet.',
-            'body'=>'<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent semper, eros id placerat auctor, metus lorem sodales justo, non facilisis sapien enim sit amet tortor. Phasellus sagittis erat blandit, ultrices eros vel, ultrices mi. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vestibulum ipsum mauris, venenatis non condimentum a, ultricies quis diam. Etiam commodo sodales tellus id faucibus. Maecenas dignissim, justo vel cursus viverra, felis augue lacinia eros, id ullamcorper erat ex a mi. Mauris non nulla dignissim quam ultrices tincidunt vitae ac elit.</p>',
+        Post::factory(5)->create([
+            'user_id'=>$user->id
         ]);
     }
 }
--- docs/4. working-with-databases/Turbo-Boost-With-Factories.md
+++ docs/4. working-with-databases/Turbo-Boost-With-Factories.md
@@ -1,1 +1,74 @@
 [< Volver al índice](../index.md)
+
+# Turbo Boost with Factories
+
+Las fabricas son un patron de diseño utilizado para crear objetos de una manera rapida y sencilla. En nuestro caso para generar datos fake para pruebas en nuestra base de datos.
+
+- Primeramente vamos a ejecutar el siguiente comando:
+
+```bash
+php artisan make:factory PostFactory
+php artisan make:factory CategoryFactory
+```
+
+Lo anterior para que nos genere el archivo **_PostFactory.php_** en la ruta `database/factories`.
+
+Luego creamos un model para comments con el comando:
+
+
+```bash
+php artisan make:model Comments -mf
+```
+El **_-mf_** nos permite generar de una vez el archivo model y el factory.
+
+Le realizamos un reset a la base de datos con `php artisan migrate:fresh`
+
+## Modificacion de archivos Factory y Seeder
+
+#### CategoryFactory.php
+Se definen los tipos de datos de las entidades a generar.+
+```php
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->word,
+            'slug' => $this->faker->slug,
+        ];
+    }
+
+```
+
+#### PostFactory.php
+Se definen los tipos de datos de las entidades a generar.
+
+```php
+    public function definition()
+    {
+        return [
+            'user_id' => User::factory(),
+            'category_id' => Category::factory(),
+            'slug' => $this->faker->slug,
+            'title' => $this->faker->sentence,
+            'excerpt' => $this->faker->sentence,
+            'body' => $this->faker->paragraph,
+        ];
+    }
+
+```
+
+#### DatabaseSeeder.php
+Se modifica el codigo para que cuando se ejecute el comando de seed cree 5 posts pero se relacionen que el usuario ya creado igualmente con favtory
+```php
+    public function run()
+    {
+       $user = User::factory()->create([
+        'name' => 'John Doe'
+       ]);
+
+        Post::factory(5)->create([
+            'user_id'=>$user->id
+        ]);
+    }
+```
+
+Finalmente hacemos un `php artisan db:seed`
