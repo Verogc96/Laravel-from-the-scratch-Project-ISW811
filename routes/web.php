961c53761ecb6e17787785833e36e2d0db594279
Veronica Gutierrez <verogc96@gmail.com>
2024-06-24 00:08:09 -0600

    Author Filtering

--- routes/web.php
+++ routes/web.php
@@ -8,32 +8,8 @@ use App\Models\User;
 use Illuminate\Support\Facades\File;
 use Spatie\YamlFrontMatter\YamlFrontMatter;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', [PostController::class, 'index'])->name('home');
 
 Route::get('/posts/{post:slug}', [PostController::class, 'show']);
 
-Route::get('/authors/{author}', function (User $author) {
-    return view ('posts', [
-        'posts'=> $author->posts
-    ]);
-});
-
-
-/*Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');*/
