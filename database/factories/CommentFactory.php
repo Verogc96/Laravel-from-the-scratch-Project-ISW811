89ebf44fc0a4c98d526b7cf0d33871b4b6dda072
Veronica Gutierrez <verogc96@gmail.com>
2024-07-01 22:25:22 -0600

    Make the Comments Section Dynamic

--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -2,10 +2,15 @@
 
 namespace Database\Factories;
 
+use App\Models\Comment;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class CommentFactory extends Factory
 {
+
+    protected $model = Comment::class;
     /**
      * Define the model's default state.
      *
@@ -14,7 +19,9 @@ class CommentFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'post_id' => Post::factory(),
+            'user_id' => User::factory(),
+            'body' => $this->faker->paragraph()
         ];
     }
 }
